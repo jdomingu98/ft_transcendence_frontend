68dc2ee521c6f9025e4512f03ea0f68cba15e6f2
Alejandro Ruzafa <ruzafa-@outlook.es>
2024-06-30 15:50:00 +0200

    lib: add eslint package and config

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import js from '@eslint/js'
+import globals from 'globals'
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+      }
+    }
+  }
+]
