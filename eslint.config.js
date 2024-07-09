e1d0aab5366cbeb88a58c685cda789e2f897d8da
Alejandro Ruzafa <ruzafa-@outlook.es>
2024-07-09 14:28:51 +0200

    lib: add eslint configuration
    add custom rules and ignore libs

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,23 @@ export default [
       sourceType: 'module',
       globals: {
         ...globals.browser,
-      }
+      },
+    },
+    ignores: ["lib/"],
+    rules: {
+      "no-else-return": "error",
+      "no-empty": "error",
+      "no-empty-function": "error",
+      "no-empty-static-block": "error",
+      "no-var": "error",
+      "prefer-const": "error",
+      "sort-imports": "error",
+      "strict": "error",
+      "no-console": "error",
+      "no-debugger": "error",
+      "no-eval": "error",
+      "semi": "error",
+      "eqeqeq": "error",
     }
   }
 ]
