4d5c7316797e9e5a3bf039b58db29065fa83f952
cmorales <moralesrojascr@gmail.com>
2024-07-25 18:03:10 +0200

    fix: import missing styles

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
-import '/src/bootstrap.scss';
-import './pages';
 import WebComponent, { Router } from '#WebComponent';
+
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css?inline';
 import style from './styles.css?inline';
+
+import '/src/bootstrap.scss';
+import './pages';
+import '#common';
 
 
 WebComponent.defineGlobalCSS([style, bootstrap]);
 
 const routes = [
-    { path: '/', component: 'landing-page' },
-    { path: '/home', component: 'home-page'},
-    { path: '/profile/:id', component: 'profile-page'},
+  { path: '/', component: 'landing-page' },
+  { path: '/home', component: 'home-page'},
+  { path: '/profile/:id', component: 'profile-page'},
+  { path: '/modal', component: 'modal-1'},
 ];
 
 Router.initRouter(routes);
