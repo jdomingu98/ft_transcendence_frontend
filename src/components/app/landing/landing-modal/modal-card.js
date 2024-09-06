40ae123e40d6a111904c43c5d4423150928cd535
cmorales <moralesrojascr@gmail.com>
2024-09-06 19:16:48 +0200

    refactor(register-modal): split code into generic modal and register-section

---
+++ src/components/app/landing/landing-modal/modal-card.js
@@ -0,0 +1,61 @@
+import '/src/components/app/landing';
+
+import './register-section/register-section';
+import WebComponent, { Component } from '#WebComponent';
+
+import css from './modal-card.css?inline';
+
+
+
+export default Component(
+    {
+        tagName: 'modal-card',
+        styleCSS: css,
+    },
+
+    class ModalCard extends WebComponent {
+
+        init() {
+            this.state = {
+                submitting: false,
+                showRegister: true,
+                errors: {}
+            };
+        }
+
+        closeModal() {
+            this.setState({
+                ...this.state,
+                showRegister: false,
+            });
+        }
+
+        closeEsc = (e) => {
+            if (e.key === 'Escape') {
+                this.emit('CLOSE_MODAL');
+            }
+        };
+
+        bind() {
+            this.subscribe('.overlay', 'click', () => this.emit('CLOSE_MODAL'));
+
+            this.subscribe('register-section', 'CLOSE_MODAL', () => this.emit('CLOSE_MODAL'));
+
+            document.addEventListener('keydown', this.closeEsc);
+            return () => {
+                document.removeEventListener('keydown', this.closeEsc);
+            };
+        }
+
+        render() {
+            return `
+                <div class="overlay"></div>
+                <div class="modal-form">     
+                    <div class="img-section"></div>
+                        <register-section></register-section>
+                    </div>
+                </div>
+            `;
+        }
+    }
+);
