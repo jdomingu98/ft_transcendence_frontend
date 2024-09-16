1a9076443eddab14c6c862b4214d9192791315e1
cmorales <moralesrojascr@gmail.com>
2024-09-16 17:42:01 +0200

    refactor(close-modal): change svg to icon

--- src/components/svg/index.js
+++ src/components/svg/index.js
@@ -1,9 +1,7 @@
 import LandingLaptop from './landing-laptop.js';
 import LandingMailbox from './landing-mailbox.js';
 import LandingMobile from './landing-mobile.js';
-import closeModal from './close-modal.js';
 export {
-    closeModal,
     LandingLaptop,
     LandingMobile,
     LandingMailbox,
