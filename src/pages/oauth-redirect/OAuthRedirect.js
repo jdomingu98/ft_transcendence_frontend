6bb12c276946feb9c5cb390e8cedf98b9ce710fc
Alejandro Ruzafa <ruzafa-@outlook.es>
2024-09-23 17:52:12 +0200

    feat: improve oauth page
    - Added redirections
    - Added toast for testing reason (will be deleted)

--- src/pages/oauth-redirect/OAuthRedirect.js
+++ src/pages/oauth-redirect/OAuthRedirect.js
@@ -2,6 +2,7 @@ import '../../components/common/spinner/app-spinner';
 import WebComponent, { Component, Router } from '#WebComponent';
 import AuthService from '#services/AuthService';
 import NavigatorService from '#services/NavigatorService';
+import { ToastService } from '#services/ToastService.js';
 import css from './OAuthRedirect.css?inline';
 
 
@@ -18,9 +19,29 @@ class OAuthRedirect extends WebComponent {
             //     .then(() => console.log('@TODO: set token in local storage'))
             //     .catch(() => NavigatorService.goToErrorPage('Failed to login'));
             AuthService.oauthLogin(code)
-                .then(console.log)
-                .catch(console.error);
+                .then(() => NavigatorService.goToHome())
+                .catch(({ error }) => NavigatorService.goToErrorPage(error));
+                // .catch(() => {
+                //     ToastService.addToast({
+                //         title: 'Login con 42',
+                //         body: 'Ud. se ha logueado exitosamente',
+                //     });
+                //     NavigatorService.goToHome();
+                // });
+                // .catch(() => NavigatorService.goToErrorPage('OAUTH.REDIRECT.ERROR'));
+                // .catch(() => NavigatorService.goTo404Page());
+        } else {
+            NavigatorService.goToErrorPage('OAUTH.REDIRECT.NO_CODE');
         }
+    }
+
+    bind() {
+        this.subscribe('button', 'click', () => {
+            ToastService.addToast({
+                title: 'Success',
+                body: 'You have logged in successfully',
+            });
+        });
     }
 
     render() {
@@ -33,6 +54,7 @@ class OAuthRedirect extends WebComponent {
                         <h5 class="card-title">Iniciando sesión con 42.</h5>
                         <p class="card-text">Por favor, tenga muchisima paciencia, sus servidores son lentos.</p>
                         <app-spinner></app-spinner>
+                        <button class="btn btn-primary mt-3">Ingresar</button>
                     </div>
                 </div>
                 <div id="footer-gradient" class="col-12"></div>
