2d5839000518c992a778e2acc3146ebe5c72e6e6
Alejandro Ruzafa <ruzafa-@outlook.es>
2024-09-23 18:43:49 +0200

    feat: store tokens in local storage

--- src/pages/oauth-redirect/OAuthRedirect.js
+++ src/pages/oauth-redirect/OAuthRedirect.js
@@ -15,33 +15,17 @@ class OAuthRedirect extends WebComponent {
         const queries = Router.getQuery();
         const { code } = queries;
         if (code) {
-            // AuthService.oauthLogin(code)
-            //     .then(() => console.log('@TODO: set token in local storage'))
-            //     .catch(() => NavigatorService.goToErrorPage('Failed to login'));
             AuthService.oauthLogin(code)
-                .then(() => NavigatorService.goToHome())
+                .then(({ access_token, refresh_token }) => {
+                    localStorage.setItem('access_token', access_token);
+                    localStorage.setItem('refresh_token', refresh_token);
+                    ToastService.addToast({ title: 'Login con 42', body: 'Ud. se ha logueado exitosamente' });
+                    NavigatorService.goToHome();
+                })
                 .catch(({ error }) => NavigatorService.goToErrorPage(error));
-                // .catch(() => {
-                //     ToastService.addToast({
-                //         title: 'Login con 42',
-                //         body: 'Ud. se ha logueado exitosamente',
-                //     });
-                //     NavigatorService.goToHome();
-                // });
-                // .catch(() => NavigatorService.goToErrorPage('OAUTH.REDIRECT.ERROR'));
-                // .catch(() => NavigatorService.goTo404Page());
         } else {
             NavigatorService.goToErrorPage('OAUTH.REDIRECT.NO_CODE');
         }
-    }
-
-    bind() {
-        this.subscribe('button', 'click', () => {
-            ToastService.addToast({
-                title: 'Success',
-                body: 'You have logged in successfully',
-            });
-        });
     }
 
     render() {
@@ -54,7 +38,6 @@ class OAuthRedirect extends WebComponent {
                         <h5 class="card-title">Iniciando sesión con 42.</h5>
                         <p class="card-text">Por favor, tenga muchisima paciencia, sus servidores son lentos.</p>
                         <app-spinner></app-spinner>
-                        <button class="btn btn-primary mt-3">Ingresar</button>
                     </div>
                 </div>
                 <div id="footer-gradient" class="col-12"></div>
