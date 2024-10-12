eb24d6aa9d8395999077d0048cdd0941cbc8b673
jdomingu98 <jadominguezarcas@gmail.com>
2024-10-12 16:43:46 +0200

    feat(settings): add rest of submenu links

--- src/pages/settings/SettingsPage.js
+++ src/pages/settings/SettingsPage.js
@@ -59,13 +59,42 @@ class SettingsPage extends WebComponent {
     bind() {
         this.subscribeAll('.options-settings div', 'click', e => {
             this._getDOM().querySelectorAll('.options-settings div').forEach(opt => opt.classList.remove('selected'));
-            console.log(e.currentTarget);
             e.currentTarget.classList.add('selected');
         });
 
         this.subscribe('#settings-user-management', 'click', () => {
             const userManagement = this._getDOM().querySelector('settings-user-management');
             const divToScroll = userManagement.shadowRoot.getElementById('user-management');
+            divToScroll.scrollIntoView({ behavior: 'smooth' });
+        });
+
+        this.subscribe('#settings-change-password', 'click', () => {
+            const changePassword = this._getDOM().querySelector('settings-change-password');
+            const divToScroll = changePassword.shadowRoot.getElementById('change-password');
+            divToScroll.scrollIntoView({ behavior: 'smooth' });
+        });
+
+        this.subscribe('#settings-two-factor', 'click', () => {
+            const twoFactor = this._getDOM().querySelector('settings-two-factor');
+            const divToScroll = twoFactor.shadowRoot.getElementById('two-factor');
+            divToScroll.scrollIntoView({ behavior: 'smooth' });
+        });
+
+        this.subscribe('#settings-visibility', 'click', () => {
+            const visibility = this._getDOM().querySelector('settings-visibility');
+            const divToScroll = visibility.shadowRoot.getElementById('visibility');
+            divToScroll.scrollIntoView({ behavior: 'smooth' });
+        });
+
+        this.subscribe('#settings-about', 'click', () => {
+            const about = this._getDOM().querySelector('settings-about');
+            const divToScroll = about.shadowRoot.getElementById('about');
+            divToScroll.scrollIntoView({ behavior: 'smooth' });
+        });
+
+        this.subscribe('#settings-delete-account', 'click', () => {
+            const deleteAccount = this._getDOM().querySelector('settings-delete-account');
+            const divToScroll = deleteAccount.shadowRoot.getElementById('delete-account');
             divToScroll.scrollIntoView({ behavior: 'smooth' });
         });
     }
@@ -81,11 +110,11 @@ class SettingsPage extends WebComponent {
                 <div class="d-flex col justify-content-center align-items-center">
                     <div class="d-flex row">
                         <settings-user-management id="user-management"></settings-user-management>
-                        <!--<settings-change-password></settings-change-password>
-                        <settings-two-factor></settings-two-factor>
-                        <settings-visibility></settings-visibility>
-                        <settings-about></settings-about>
-                        <settings-delete-account></settings-delete-account> -->
+                        <!--<settings-change-password id="change-password"></settings-change-password>
+                        <settings-two-factor id="two-factor"></settings-two-factor>
+                        <settings-visibility id="visibility"></settings-visibility>
+                        <settings-about id="about"></settings-about>
+                        <settings-delete-account id="delete-account"></settings-delete-account> -->
                     </div>
                 </div>
             </div>
