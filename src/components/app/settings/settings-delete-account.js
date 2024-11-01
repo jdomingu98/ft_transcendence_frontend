f8668a20575fb92515f130b9af732b81ecd1ab0a
jdomingu98 <jadominguezarcas@gmail.com>
2024-11-01 16:26:39 +0100

    fix(settings-delete-account): add section text

--- src/components/app/settings/settings-delete-account.js
+++ src/components/app/settings/settings-delete-account.js
@@ -12,18 +12,17 @@ class SettingsDeleteAccount extends WebComponent {
 
     render() {
         return `
-            <div class="my-5 row">
+            <div class="mt-3 mb-5 row">
                 <div id='${this.id}'>
                     <h2-text color="var(--app-secondary-color)">Delete Account</h2-text>
                 </div>
-                <div class="mt-3 mb-5 text-white" style="width:85%; font-size: 1.2rem">
-                    <p class="paragraph mb-4">We strongly recommend you to activate the two-factor authenticacion method to protect your account even further.</p>
-                    <p class="paragraph mb-4">By selecting or deselecting the checkbox, the change request will be applied instantly.</p>
-                    <p class="paragraph mb-4">In case of activation, the email provided in the registration form will be used to send the security codes.</p>
+                <div class="my-4 text-white" style="width:85%; font-size: 1.2rem">
+                    <p class="paragraph mb-4">We are sorry if something wasn’t to your liking.</p>
+                    <p class="paragraph mb-4">Before you leave the site, please note that you will need to re-register to access the site again, as all your details will be deleted.</p>
+                    <p class="paragraph mb-5">Hope we will see you soon in the future,</p>
+                    <p class="paragraph">The Transcendence team</p>
                 </div>
-                <div>
-                    <button class="primary-red-btn primary-btn px-0 border-0 fw-bold text-uppercase rounded-pill">Delete account permanently</button>
-                </div>
+                <button class="primary-red-btn primary-btn px-0 border-0 fw-bold text-uppercase rounded-pill">Delete account permanently</button>
             </div>
         `;
     }
