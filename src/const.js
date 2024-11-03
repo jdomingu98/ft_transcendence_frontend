b7e15279ebde412facf15d50ab2b94c03ba7c165
jdomingu98 <jadominguezarcas@gmail.com>
2024-11-03 22:42:54 +0100

    refactor(const.js): update some values

--- src/const.js
+++ src/const.js
@@ -11,27 +11,28 @@ const BUTTON_DEFAULT_SECONDARY_COLOR = 'var(--app-secondary-color)';
 
 const DEFAULT_PROFILE_IMG = 'https://placehold.co/100/png';
 
-const DEFAULT_BANNER_IMG = 'https://placehold.co/1000x200/png';
+const DEFAULT_BANNER_IMG = '/src/resources/default-banner.webp';
 
 const DEFAULT_SIDEBAR_PROFILE_IMG = 'https://placehold.co/50/png';
 
-export const LANGUAGES = [
+const LANGUAGES = [
     { language: 'es', flag:  '🇪🇸', url: '/src/i18n/es.json', name: 'Español', default: true },
-    { language: 'en', flag:  '🇺🇸', url: '/src/i18n/en.json', name: 'English'},
-    { language: 'fr', flag:  '🇫🇷', url: '/src/i18n/fr.json', name: 'Français'},
-    { language: 'zh', flag:  '🇨🇳', url: '/src/i18n/zh.json', name: '简体中文'}
+    { language: 'en', flag:  '🇺🇸', url: '/src/i18n/en.json', name: 'English' },
+    { language: 'fr', flag:  '🇫🇷', url: '/src/i18n/fr.json', name: 'Français' },
+    { language: 'zh', flag:  '🇨🇳', url: '/src/i18n/zh.json', name: '简体中文' }
 ];
 
 const DECISION_THRESHOLD = 300;
 
 export {
     BUTTON_DEFAULT_MSG,
-    DEFAULT_DIMENSION_VALUE,
-    DEFAULT_IMG_DIMENSION,
     BUTTON_DEFAULT_PRIMARY_COLOR,
     BUTTON_DEFAULT_SECONDARY_COLOR,
-    DEFAULT_PROFILE_IMG,
+    DECISION_THRESHOLD,
     DEFAULT_BANNER_IMG,
+    DEFAULT_DIMENSION_VALUE,
+    DEFAULT_IMG_DIMENSION,
+    DEFAULT_PROFILE_IMG,
     DEFAULT_SIDEBAR_PROFILE_IMG,
-    DECISION_THRESHOLD
+    LANGUAGES
 };
