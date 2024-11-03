83269dc7b655212bc943ae91a717cd98f4bf6be8
Alejandro Ruzafa <ruzafa-@outlook.es>
2024-11-03 20:28:46 +0100

    feat: set image aspect-ratio

--- src/pages/search-results/SearchResults.js
+++ src/pages/search-results/SearchResults.js
@@ -1,4 +1,5 @@
 import WebComponent, { Component, Router } from '#WebComponent';
+import { DEFAULT_PROFILE_IMG } from '#const';
 import FetchService from '#services/UserService';
 
 export default Component ({
@@ -18,7 +19,7 @@ class SearchResults extends WebComponent {
             <div class="col-md-3 col-sm-6 mb-4">
                 <router-link href="/app/profile/${result.id}">
                     <div class="card mt-3" style="background-color: var(--app-secondary-bg-color)">
-                        <img src="${result.profile_img}" alt="${result.username}" style="object-fit: cover">
+                        <img src="${result.profile_img ?? DEFAULT_PROFILE_IMG}" alt="${result.username}" style="object-fit: cover; aspect-ratio: 4/3">
                         <div class="card-body p-2">
                             <h5 class="card-title text-truncate text-white text-center" style="margin-top: 0.5rem">${result.username}</h5>
                         </div>
