e7899eb1d24b7f8aa9aa556a065c3abfc130e74e
jdomingu98 <jadominguezarcas@gmail.com>
2024-11-07 18:27:39 +0100

    feat(OAuthRedirect): add OTP security step
    Also fix missing literal in redirect snackbar

--- src/pages/oauth-redirect/OAuthRedirect.js
+++ src/pages/oauth-redirect/OAuthRedirect.js
@@ -9,24 +9,44 @@ export default Component({
     styleCSS: css,
 },
 class OAuthRedirect extends WebComponent {
+
     init() {
         const { code } = Router.getQuery();
         if (!code)
             NavigatorService.goToErrorPage('ERROR.OAUTH.NO_CODE');
         else {
             AuthService.oAuthLogin(code)
-                .then(({ access_token, refresh_token }) => {
-                    localStorage.setItem('access_token', access_token);
-                    localStorage.setItem('refresh_token', refresh_token);
-                    SnackbarService.addToast({ title: 'Login con 42', body: 'Ud. se ha logueado exitosamente' });
-                    NavigatorService.goToHome();
+                .then(({ access_token, refresh_token, two_factor_enabled }) => {
+                    two_factor_enabled
+                        ? this.showOTPModal('username')
+                        : this.loginSuccessfully(access_token, refresh_token);
                 })
                 .catch(({ error }) => NavigatorService.goToErrorPage(error));
         }
     }
 
+    loginSuccessfully(access_token, refresh_token) {
+        localStorage.setItem('access_token', access_token);
+        localStorage.setItem('refresh_token', refresh_token);
+        SnackbarService.addToast({
+            title: 'SNACKBAR.OAUTH_REDIRECT.TITLE',
+            body: 'SNACKBAR.OAUTH_REDIRECT.DESC'
+        });
+        NavigatorService.goToHome();
+    }
+
+    showOTPModal(username) {
+        const otpModal = this._getDOM().querySelector('landing-otp-modal');
+        otpModal.openModal(username);
+    }
+
+    afterViewInit() {
+        this.subscribe('landing-auth-modal', 'OPEN_OTP', ({detail}) => this.showOTPModal(detail));
+    }
+
     render() {
         return `
+            <landing-otp-modal></landing-otp-modal>
             <div class="oauth-container position-relative overflow-hidden w-100 d-flex align-items-end">
                 <div class="col-12 p-0 position-absolute rounded-circle lights top-light"></div>
                 <div class="col-12 p-0 position-absolute rounded-circle lights middle-light"></div>
