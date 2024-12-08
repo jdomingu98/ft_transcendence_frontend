6103d0345e2033bd212bb864bac57c699611d7eb
jdomingu98 <jadominguezarcas@gmail.com>
2024-12-09 00:04:04 +0100

    feat(game): add modals to game outside logged in users

--- src/pages/pong-page/PongPage.js
+++ src/pages/pong-page/PongPage.js
@@ -10,12 +10,27 @@ export default Component({
 },
 class PongPage extends WebComponent {
 
-    #firstTimePaused = false
+    #firstTimePaused = false;
 
     init() {
         this.state = {
             accessToken: localStorage.getItem('access_token'),
-            isTournament: window.location.pathname.includes('tournament')
+            isTournament: window.location.pathname.includes('tournament'),
+            stop: true,
+            isRegistrationOpen: true,
+            playerOne: null,
+            playerTwo: null,
+            winner: '',
+            isMatchOver: false,
+            numGoalsScored: 0,
+            numGoalsAgainst: 0,
+            winnerModalConfig: {
+                label: {
+                    title: 'WINNER_MODAL.LOCAL_MATCH.TITLE',
+                    description: 'WINNER_MODAL.LOCAL_MATCH.DESC',
+                    button: 'WINNER_MODAL.LOCAL_MATCH.BUTTON',
+                }
+            }
         };
     }
 
@@ -32,12 +47,38 @@ class PongPage extends WebComponent {
     bind() {
         this.subscribe('landing-navbar', 'OPEN_MODAL', () => this.showModal('LOGIN'));
         this.subscribe('landing-auth-modal', 'OPEN_OTP', ({ detail }) => this.showOTPModal(detail));
+
+        this.subscribe('local-match-registration-modal', 'START_LOCAL_MATCH', ({detail}) => {
+            Sounds.startBackgroundMusic();
+            this.setState({
+                ...this.state,
+                stop: false,
+                isRegistrationOpen: false,
+                playerOne: detail.playerOne,
+                playerTwo: detail.playerTwo,
+            });
+        });
+
+
         this.subscribe('app-game', 'ON_PAUSE', ({ detail: paused }) => {
             if (!paused && !this.#firstTimePaused) {
                 this.#firstTimePaused = true;
                 Sounds.startBackgroundMusic();
             }
-        })
+        });
+
+        this.subscribe('app-game', 'FINISH_GAME', ({ detail }) => {
+            this.setState({
+                ...this.state,
+                stop: true,
+                isMatchOver: true,
+                winner: detail.winner,
+                playerOne: detail.playerOne,
+                playerTwo: detail.player,
+                numGoalsScored: detail.numGoalsScored,
+                numGoalsAgainst: detail.numGoalsAgainst,
+            });
+        });
     }
 
     onDestroy() {
@@ -45,19 +86,30 @@ class PongPage extends WebComponent {
     }
 
     render() {
-        if (this.state.accessToken)
+        if (this.state.accessToken) {
             NavigatorService.goToSidebarElementPage(this.state.isTournament ? 'tournament': 'game');
+            return;
+        }
         return `
-            <div class="h-100 w-100" style="max-height: 100vh">
-                <landing-auth-modal></landing-auth-modal>
-                <landing-otp-modal></landing-otp-modal>
-                <landing-navbar></landing-navbar>
-                <section class="d-flex justify-content-center align-items-center" style="background-color: var(--app-primary-bg-color)">
-                    <div style="max-width: 70%">
-                        <app-game [useRandomPlayers]="true"></app-game>
-                    </div>
-                </section>
-            </div>
+            <landing-auth-modal></landing-auth-modal>
+            <landing-otp-modal></landing-otp-modal>
+            <landing-navbar></landing-navbar>
+            <local-match-registration-modal [open]="state.isRegistrationOpen"></local-match-registration-modal>
+            ${this.state.isMatchOver ? `
+                <winner-modal [winner]="state.winner" [config]="state.winnerModalConfig"></winner-modal>
+            ` : ''}
+            <section class="d-flex justify-content-center align-items-center" style="background-color: var(--app-primary-bg-color)">
+                <div style="max-width: 70%">
+                    <app-game
+                        [useRandomPlayers]="true"
+                        [isStopped]="state.stop"
+                        [playerOne]="state.playerOne"
+                        [playerTwo]="state.playerTwo"
+                        [numGoalsScored]="state.numGoalsScored"
+                        [numGoalsAgainst]="state.numGoalsAgainst"
+                    ></app-game>
+                </div>
+            </section>
         `;
     }
 });
